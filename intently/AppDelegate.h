f623466b2477e1ebf2492680284f96326af0ec1c
jimtoepel <jim.toepel@gmail.com>
2015-03-18 20:13:02 -0700

    Initial Commit

---
+++ intently/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  intently
+//
+//  Created by Jim Toepel on 3/18/15.
+//  Copyright (c) 2015 FunderDevelopment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ intently/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  intently
+//
+//  Created by Jim Toepel on 3/18/15.
+//  Copyright (c) 2015 FunderDevelopment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
